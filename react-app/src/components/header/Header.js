27145aa8d053410a272e26e179954eb460e54342
David J <davidhjolley2000@gmail.com>
2023-03-21 12:00:05 -0400

    Adds linking to the JSON and cards

--- react-app/src/components/header/Header.js
+++ react-app/src/components/header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { NavLink } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 function Header() {
     return (
